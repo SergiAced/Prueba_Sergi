9f34ba2874a1230cd2d583f3af2ce1635fa419ff
German Caballero <gcaballero@pronoide.es>
2017-06-28 17:00:32 +0200

    Commit Inicial

---
+++ AppConsolaEjemplosPOO_2/AppConsolaEjemplosPOO_2/Module1.vb
@@ -0,0 +1,55 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim rec_1 As Rectangulo = New Rectangulo(5, 6)
+        rec_1.Mostrar()
+        Dim circ As Circulo = New Circulo(2, AddressOf CalculandoAreaCirculo)
+        circ.Mostrar()
+
+        Dim rec_2 As Figura = New Rectangulo(2, 3)
+        Dim cir_2 As Figura = New Circulo(3, AddressOf CalculandoAreaCirculo)
+
+        rec_2.Mostrar()
+        cir_2.Mostrar()
+        Console.WriteLine("Perimetro Decimal cir_2 es " & cir_2.Perimetro)
+
+        ' no se puede instanciar una clase abastraccta
+        ' Dim fig As Figura = New Figura()
+
+        Try
+            Dim cir_2_circ As Circulo
+            cir_2_circ = CType(rec_2, Circulo)
+            cir_2_circ.Mostrar()
+        Catch ex As InvalidCastException
+            Console.WriteLine("Conversion NO VALIDA")
+        Catch ex As Exception
+            Console.WriteLine("Error de tipo " & ex.Message)
+        Finally
+            Console.WriteLine("SIEMPRE SE EJECTUA AL TERMINAR")
+        End Try
+
+        Dim miVinoDonSimon = New Tetrabrik(7, 15, 5)
+
+        Dim elementosConArea() As IConAreaCalculable
+
+        elementosConArea = New IConAreaCalculable() {
+            rec_1,
+            rec_2,
+            circ,
+            miVinoDonSimon
+            }
+        For i = 0 To 3
+            Console.WriteLine("Area calculada " & elementosConArea(i).Area)
+        Next
+        Console.ReadKey()
+    End Sub
+    Sub CalculandoAreaCirculo(cadena As String)
+        Console.WriteLine("Calculando Area Circulo: " & cadena)
+
+    End Sub
+
+    Sub CalcularPerimetroFigura(i As Integer)
+        Console.WriteLine("Calculando Area Circulo: " & cadena)
+
+    End Sub
+End Module
---
+++ AppConsolaEjemplosPOO_2/AppConsolaEjemplosPOO_2/Tetrabrik.vb
@@ -0,0 +1,23 @@
+﻿Imports AppConsolaEjemplosPOO_2
+
+Public Class Tetrabrik
+    Implements IConAreaCalculable
+
+    Public an, al, pr As Single
+
+    Public Sub New(an As Single, al As Single, pr As Single)
+        Me.an = an
+        Me.al = al
+        Me.pr = pr
+    End Sub
+
+    Public ReadOnly Property Area As Single Implements IConAreaCalculable.Area
+        Get
+            Return CalcularArea()
+        End Get
+    End Property
+
+    Public Function CalcularArea() As Single Implements IConAreaCalculable.CalcularArea
+        Return Me.an * Me.al * 2 + Me.pr * Me.al * 2 + Me.pr * Me.an * 2
+    End Function
+End Class
